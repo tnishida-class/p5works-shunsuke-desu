b2beb4077a44f1610306585c76c668d3ea22d209
shunsuke-desu <72760987+shunsuke-desu@users.noreply.github.com>
2020-10-24 17:17:25 +0900

    change section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,14 @@
 // 小手調べ
 function setup(){
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    noFill();
+    if(i < 5){
+      stroke(0, 0, 255);
+    }
+    else{
+      stroke(255, 0, 0);
+    }
+    ellipse(50, 50, 8*i + 10);
+
   }
 }
