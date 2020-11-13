6de63a07b2af115d4bd17f4bab35f923fe07f6b6
shunsuke-desu <72760987+shunsuke-desu@users.noreply.github.com>
2020-11-14 02:19:02 +0900

    complete section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -6,22 +6,39 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
-      fill(i % 2 == 1 && j % 2 == 1 || i % 2 == 0 && j % 2 == 0 ?  255 : 150);
-      console.log(i, j);
-      rect(i * 10, j * 10, 10 , 10);
+      if((i + j) % 2 == 0){
+        fill(255, 255, 255);
+        rect(i * 10, j * 10, 10 , 10);
+      }
+      else{
+        fill(130, 130, 130);
+        rect(i * 10, j * 10, 10 , 10);
+      }
     }
   }
-  for(let k = 0; k < 8; k++){
-    for(let l = 0; l < 8; l++){
-      console.log(k, l);
-      if(k % 2 == 0 && l % 2 == 1 || k % 2 == 0 && l % 2 == 1, l <= 2){
+  for(let m = 0; m < 8; m++){
+    for(let n = 0; n < 8; n++){
+      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      if(n < 3){
         fill(255, 0, 0);
-        ellipse(k * 10 + 5, l * 10 + 5, 9);
+        ellipse(m * 10 + 5, n * 10 + 5, 8);
       }
-      else if(k % 2 == 1 && l % 2 == 1 || k % 2 == 0 && l % 2 == 0, l >= 5){
-        fill(0);
-        ellipse(k * 10 + 5, l * 10 + 5, 9);
-      }
+      else if(n > 4){
+        fill(0, 0, 0);
+        ellipse(m * 10 + 5, n * 10 + 5, 8);
       }
     }
   }
+  for(let a = 0; a < 8; a++){
+    for(let b = 0; b < 8; b++){
+      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      if((a + b) % 2 == 0){
+        fill(255, 255, 255);
+        rect(a * 10, b * 10, 10 , 10);
+      }
+      else{
+        noFill();
+      }
+    }
+  }
+}
