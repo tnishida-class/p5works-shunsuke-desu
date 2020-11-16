519ada322eae09d704a96f781a47a93cd0a78b1e
shunsuke-desu <72760987+shunsuke-desu@users.noreply.github.com>
2020-11-16 15:35:28 +0900

    1st change

--- section3-1/sketch.js
+++ section3-1/sketch.js
@@ -2,11 +2,24 @@
 // 練習問題：このプログラムを改造してEUの旗を描いてみよう
 function setup(){
   createCanvas(200, 200);
-  background(192);
+  background(50, 60, 230);
+  noStroke();
+  fill(250, 250, 50)
   for(let i = 0; i < 12; i++){
     let theta = TWO_PI * i / 12;
     let x = 100 + cos(theta) * 50;
     let y = 100 + sin(theta) * 50;
-    ellipse(x, y, 10);
+    star(x, y, 10);
   }
+}
+
+function star(cx, cy, r){
+  beginShape();
+  for(var i = 0; i < 5; i++){
+    let theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    let x = cx + cos(theta) * r;
+    let y = cy + sin(theta) * r;
+    vertex(x,y);
+  }
+  endShape(CLOSE);
 }
