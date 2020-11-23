31e46dc92be1fed0bedea2cabfbf8131364dd7a1
shunsuke-desu <72760987+shunsuke-desu@users.noreply.github.com>
2020-11-24 01:31:28 +0900

    2nd change 4-q1

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -1,16 +1,22 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
-let count;
-let cycle;
+let count, cycle, size;
 
 function setup(){
   createCanvas(200, 200);
   count = 0;
-  cycle = 10;
+  cycle = 150;
+  size = 20;
 }
 
 function draw(){
   background(160, 192, 255);
-  count = (count + 1 / 10) % cycle;
+  count = (count + 1) % cycle
+  if(count < 75){
+    size = count + 20
+  }
+  else{
+    size = 170 - count;
+  }
+  ellipse(width / 2, height / 2, size);
 
-  ellipse(width / 2, height / 2, cycle * count);
 }
