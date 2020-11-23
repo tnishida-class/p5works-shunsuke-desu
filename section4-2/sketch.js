6b2e472d657c1af64c6fa3112b647ebc284ed60b
shunsuke-desu <72760987+shunsuke-desu@users.noreply.github.com>
2020-11-24 00:15:47 +0900

    change 4-2 vy to vx

--- section4-2/sketch.js
+++ section4-2/sketch.js
@@ -1,34 +1,27 @@
 // テキスト「アニメーション」
 let x, y, vx, vy;
 const g = 1; // 重力加速度
-const vyMax = 30;
+const vxMax = 30;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
   x = width / 2;
   y = height / 2;
-  vx = 8;
-  vy = 8;
+  vx = 2;
+  vy = 2;
 }
 
 function draw(){
   background(160, 192, 255);
-  ellipse(x, y, 20, 20);
+  ellipse(x, y, 30);
   x += vx;
   y += vy;
-
+  vx += g;
   // 重力（コメント機能でオンオフ切り替えて実行してみましょう）
-  vy = constrain(vy + g, -vyMax, vyMax);
-
-  // 端の処理パターン (1) 反対側から出てくる
-  // if(x > width){ x = 0; }
-  // else if(x < 0){ x = width; }
-  // if(y > height){ y = 0; }
-  // if(y < 0){ y = height; }
-
+  vx = constrain(vx, -vxMax, vxMax);
 　// 端の処理パターン (2) 跳ね返る
   if(x < 0 || x > width){ vx = -1 * vx; }
-  if(y > height){ vy = -1 * vy; }
+  if(y < 0 || y > height){ vy = -1 * vy; }
   x = constrain(x, 0, width);
   y = constrain(y, 0, height);
 }
