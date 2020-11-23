f9a2293ca5379da0df49e2749c156972b50f6db7
shunsuke-desu <72760987+shunsuke-desu@users.noreply.github.com>
2020-11-24 02:45:01 +0900

    1st change4-q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -1,29 +1,39 @@
 // テキスト「アニメーション」
-let x, y, vx, vy;
+let x, y, vx, vy, cx, cy, r, angle;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
   x = width / 2;
   y = height / 2;
-  vx = 8;
-  vy = 8;
+  vx = 5;
+  vy = 2;
+  cx = 10;
+  cy = 10;
+  r = 10;
+  angle = PI
 }
 
 function draw(){
-  background(160, 192, 255);
+background(160, 192, 255);
   // BLANK[2] (hint: 作った star 関数を使います)
+ star(cx, cy, r, angle);
+ cx += vx;
+ cy += vy;
+ angle += PI * 1 / 30;
 
   // 端の処理パターン (1) 反対側から出てくる
-  if(x > width){ x = 0; }
-  else if(x < 0){ x = width; }
-  if(y > height){ y = 0; }
-  if(y < 0){ y = height; }
+  if(cx > width){ cx = 0; }
+  else if(cx < 0){ cx = width; }
+  if(cy > height){ cy = 0; }
+  if(cy < 0){ cy = height; }
 }
 
 function star(cx, cy, r, angle){
   beginShape();
+  noStroke();
+  fill(250, 250, 0);
   for(var i = 0; i < 20; i++){
-    var theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    var theta = TWO_PI * i * 2 / 5 - HALF_PI + angle;
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
